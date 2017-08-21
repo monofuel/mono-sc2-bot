9f4c15f9dce2a36f888e78ab229a3a2c98e7040a
monofuel <monofuel@japura.net>
2017-08-20 18:48:34 -0700

    handle connecting to client

--- ai/main.py
+++ ai/main.py
@@ -2,11 +2,45 @@
 
 from s2clientprotocol import sc2api_pb2
 import tensorflow as tf
+import websockets
+import asyncio
+import os
 
-if __name__ == "__main__":
+
+class ControllerConfig:
+    def __init__(self):
+        self.host = os.environ.get("STARCRAFT_HOST", "localhost")
+        self.port = os.environ.get("STARCRAFT_PORT", 5000)
+
+
+class Controller:
+    def __init__(self, config):
+        self.uri = "ws://%s:%d/sc2api" % (config.host, config.port)
+        print("connecting to SC2 URI: %s" % (self.uri))
+
+    async def connect(self):
+        self.socket = await websockets.connect(self.uri)
+        self.socket.send("foobar")
+        print('connected to sc2 api')
+
+    async def send(self, request):
+        await self.socket.send(request.SerializeToString())
+
+
+async def main():
     print("hello world")
-    request = sc2api_pb2.Request()
+
+    config = ControllerConfig()
+    controller = Controller(config)
+    await controller.connect()
+
+    createRequest = sc2api_pb2.RequestCreateGame()
+    createRequest.battlenet_map_name = "BelShirVestigeLE.SC2Map"
+    await controller.send(createRequest)
 
     hello = tf.constant('Hello, TensorFlow!')
     sess = tf.Session()
     print(sess.run(hello))
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
